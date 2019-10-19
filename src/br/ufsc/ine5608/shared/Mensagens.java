778d433bdbbd46f2c41457e19626e89d25f9548a
Rossini <rossini1408@gmail.com>
2019-10-19 13:17:16 -0300

    update messages and implement method to show joption panel message

--- src/br/ufsc/ine5608/shared/Mensagens.java
+++ src/br/ufsc/ine5608/shared/Mensagens.java
@@ -1,5 +1,7 @@
 package br.ufsc.ine5608.shared;
 
+import javax.swing.*;
+
 public class Mensagens {
 
     public static final String CARTAS_ESGOTADAS = "Não há mais cartas livres disponíveis";
@@ -7,13 +9,24 @@ public class Mensagens {
     public static final String CARTAS_SELECAO_COR_ERRADA = "As cartas devem possuir a mesma cor";
 
 
-    public static final String CONECTADO = "Conexão bem sucedida!";
+    public static final String SUCESSO_CONEXAO = "Conexão bem sucedida!";
     public static final String ERRO_CONEXAO = "Não foi possível conectar";
+    public static final String INFO_CONEXAO_PRONTA = "Você já está conectado";
+
+    public static final String SUCESSO_DESCONEXAO = "Desconexão bem sucedida";
+    public static final String INFO_DESCONEXAO_PRONTA = "Você já está desconectado";
+
+    public static final String INFO_CONEXAO_NECESSARIA = "A partida já está em andamento, ou você não está conectado";
+    public static final String INFO_CONEXAO_PERDIDA = "A conexão com o outro jogador foi perdida. Você  será desconectado da partida";
+
 
     public static final String OPERACAO_SUCESSO = "Operação bem sucedida";
     public static final String OPERACAO_ERRADA = "Operação incorreta";
 
     public static final String CAMPO_INVALIDO = "Campos inválido";
 
+    public static void mostraMensagem(String mensagem,int tipoMensagem){
+        JOptionPane.showMessageDialog(null, mensagem, "", tipoMensagem);
+    }
 
 }
